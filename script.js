c17551fb13a7f32b988750bbef51f59e02cc3550
rohithsingh1 <rohithsinghthakur3@gmail.com>
2022-12-14 00:54:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+let BusTicketApplication=document.getElementById('BusTicketApplication')
+BusTicketApplication.addEventListener('click', () => {
+    window.open('https://busticket.onrender.com','_blank')
+})
+let MusicPlayerApplication=document.getElementById('MusicPlayerApplication')
+MusicPlayerApplication.addEventListener('click', () => {
+     window.open('https://mernmusic.onrender.com','_blank')
+})
+let MovieApplication=document.getElementById('MovieApplication')
+MovieApplication.addEventListener('click', () => {
+    window.open('https://movie-mysql.netlify.app','_blank')
+})
+let KBCQuiz=document.getElementById('KBCQuiz')
+KBCQuiz.addEventListener('click', () => {
+     window.open('https://kbc.onrender.com','_blank')
+})
+let SpeedTyper=document.getElementById('SpeedTyper')
+SpeedTyper.addEventListener('click', () => {
+  window.open('https://speed-typer-game-javascript.netlify.app','_blank')  
+})
+
+let TouristPlace=document.getElementById('TouristPlace')
+TouristPlace.addEventListener('click', () => {
+    window.open('https://github.com/rohithsingh1/TouristPlaceApplication','_blank')  
+})
+let ColorGame=document.getElementById('ColorGame')
+ColorGame.addEventListener('click', () => {
+    window.open('https://color-game-application.netlify.app/','_blank')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
