7c2984555e38b0c31c10cb13674a27e4f20b0cd8
rohithsingh1 <rohithsinghthakur3@gmail.com>
2023-03-03 15:58:48 +0530

    updated script file

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 let BusTicketApplication=document.getElementById('BusTicketApplication')
 BusTicketApplication.addEventListener('click', () => {
-    window.open('https://busticket.onrender.com','_blank')
+    window.open('https://github.com/rohithsingh1/BusTicketBookingApplication', '_blank')
 })
 let MusicPlayerApplication=document.getElementById('MusicPlayerApplication')
 MusicPlayerApplication.addEventListener('click', () => {
-     window.open('https://mernmusic.onrender.com','_blank')
+    window.open('https://github.com/rohithsingh1/MERN_Music_Player', '_blank')
 })
 let MovieApplication=document.getElementById('MovieApplication')
 MovieApplication.addEventListener('click', () => {
-    window.open('https://movie-mysql.netlify.app','_blank')
+    window.open('https://movie-mysql.netlify.app', '_blank')
 })
 let KBCQuiz=document.getElementById('KBCQuiz')
 KBCQuiz.addEventListener('click', () => {
-     window.open('https://kbc.onrender.com','_blank')
+    window.open('https://kbc.onrender.com', '_blank')
 })
 let SpeedTyper=document.getElementById('SpeedTyper')
 SpeedTyper.addEventListener('click', () => {
-  window.open('https://speed-typer-game-javascript.netlify.app','_blank')  
+    window.open('https://speed-typer-game-javascript.netlify.app', '_blank')
 })
 
 let TouristPlace=document.getElementById('TouristPlace')
 TouristPlace.addEventListener('click', () => {
-    window.open('https://github.com/rohithsingh1/TouristPlaceApplication','_blank')  
+    window.open('https://github.com/rohithsingh1/TouristPlaceApplication', '_blank')
 })
 let ColorGame=document.getElementById('ColorGame')
 ColorGame.addEventListener('click', () => {
-    window.open('https://color-game-application.netlify.app/','_blank')
+    window.open('https://color-game-application.netlify.app/', '_blank')
 })
 let BasicCalculator=document.getElementById('BasicCalculator')
 BasicCalculator.addEventListener('click', () => {
-    window.open('https://basiccalculator4650.netlify.app/','_blank')
+    window.open('https://basiccalculator4650.netlify.app/', '_blank')
 })
 
 let GPSApplication=document.getElementById('GPSApplication')
